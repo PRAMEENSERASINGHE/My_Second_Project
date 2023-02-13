0cec4938b4c958241532f173ca9ade61ae176d37
PRAMEENSERASINGHE <https://github.com/PRAMEENSERASINGHE/my_project_1_.git>
2023-02-13 11:28:16 +0530

    connect_with_jenkin

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello_world!");
         System.out.println("New_Hello_world!");
-        System.out.println("connect with jenkin");
+        System.out.println("New_Hello_world!");
+        System.out.println("connect_with_jenkin");
+
     }
 }
